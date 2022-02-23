ffcce4bf3e692a3a2389b446ce0b01d2e17c572c
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-23 11:37:33 +0200

    we finish how toload static in teplte,how to loop

--- meetups/views.py
+++ meetups/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
    #return HttpResponse("<h1>welcome page here</h1>")
-   return render(request,'meetups/index.html')
+   meetups=[
+      {"title":"A first Meeting"},
+      {"title": "A second meeting"}
+   ]
+
+   return render(request,'meetups/index.html',{
+      "show_meetups":True,
+      "meetups":meetups
+   })
