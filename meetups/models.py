73e89bc5cad688d35c91c01369691b3d36c6d5a6
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-23 19:54:05 +0200

    we finish to createmodel forour database in mod.py

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,9 @@
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class Meetups(models.Model):
+    title:models.CharField(max_length=200)
+
+    #this django slugField adheres slug format
+    slug=models.SlugField(unique=True)   
+    description=models.TextField()
