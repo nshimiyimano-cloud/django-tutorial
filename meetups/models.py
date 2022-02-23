53f7357f7226d537bce77723ad3b2f2fe043fad0
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-23 21:30:55 +0200

    nowwith loction=m_item.loction able without static

--- meetups/models.py
+++ meetups/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Meetups(models.Model):
     title=models.CharField(max_length=200)
+    location=models.CharField(max_length=200)
 
     #this django slugField adheres slug format
     slug=models.SlugField(unique=True)   
