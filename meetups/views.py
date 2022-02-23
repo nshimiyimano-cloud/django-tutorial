cf5bb2e4facbc7cd1900d2d58bbefc494a5687ac
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-23 13:40:53 +0200

    all-mitupscss&addmitupinmain objinviu&x.hml chnged

--- meetups/views.py
+++ meetups/views.py
@@ -8,8 +8,29 @@ def index(request):
       {"title":"A first Meeting"},
       {"title": "A second meeting"}
    ]
+   meetups=[
+      {"title":"A first Meeting"},
+      {"title": "A second meeting"}
+   ]
+
+
+   meetupsinmain=[
+      {
+         "title":"A first Meetups",
+         "location":"new york",
+         "slug":"a-first-meetups"
+         },
+
+      {
+         "title": "A second meetups",
+         "location":"paris",
+         "slug":"a-second-meetupos"
+         }
+   ]
+   
 
    return render(request,'meetups/index.html',{
       "show_meetups":True,
-      "meetups":meetups
+      "meetups":meetups,
+      "meetupinmain":meetupsinmain
    })
