e8e9b51aff404ac423bfced64f3a16561b16f6ee
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-23 16:38:02 +0200

    use dynmicabsolute path with url&use nameofurl

--- meetups/urls.py
+++ meetups/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('meetups',views.index),
-    path('meetups/<slug:meetup_slug>',views.meetup_details)  
+    path('meetups',views.index,name='meetups'),
+    path('meetups/<slug:meetup_slug>',views.meetup_details,name='meetup-detail')  
 ]
