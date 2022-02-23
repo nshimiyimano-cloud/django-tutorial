b58d88a32d0bfa822237080413b73c9824189a90
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-23 20:31:44 +0200

    mkemgrationcrate sql to store inDB&mgratestoreinDB

--- django_course_site/settings.py
+++ django_course_site/settings.py
@@ -82,6 +82,21 @@ DATABASES = {
 }
 
 
+#DATABASES = {  
+    #'default': {  
+        #'ENGINE': 'django.db.backends.mysql',  
+        #'NAME': 'schooldb',  
+        #'USER': 'springbootuser',  
+        #'PASSWORD': 'Imbata_2',  
+        #'HOST': 'localhost',  
+       # 'PORT': '3306',  
+       # 'OPTIONS': {  
+         #   'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+       # }  
+   # }  
+#}  
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
