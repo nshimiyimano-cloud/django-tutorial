4b2e8fffb0b1c2d591fb1d9887794ec180fd369d
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-24 07:57:35 +0200

    urlpaterns=[]+sttic(setings.MEIA_URL)to showathtouse for serving image on server

--- django_course_site/urls.py
+++ django_course_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.urls import include,path
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings   # to specify folder we decided in settings.py for serveing image as static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('',include('meetups.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
