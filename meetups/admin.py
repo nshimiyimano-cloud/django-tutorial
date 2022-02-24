b71da8cc13d373f604ab9c8b9a2b0c49d9bd61a5
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-02-24 09:49:37 +0200

    wehavecreatedlocation partcular&relatinshipindjango

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Meetups
+from .models import Meetups,Location
 
 class MeetupAdmin(admin.ModelAdmin):
     list_display=('title','slug')
-    list_filter=('title',)
+    list_filter=('title','location',)
+    prepopulated_fields={'slug':('title',)}
 
 
 # Register your models here.
 
 admin.site.register(Meetups,MeetupAdmin)
+admin.site.register(Location)
--- meetups/models.py
+++ meetups/models.py
@@ -1,6 +1,14 @@
 from statistics import mode
 from django.db import models
 
+
+class Location(models.Model):   #here relationship mapping needed to map in Meetups model
+    name=models.CharField(max_length=200)
+    address=models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
 class Meetups(models.Model):
     title=models.CharField(max_length=200)
     location=models.CharField(max_length=200)
@@ -8,7 +16,8 @@ class Meetups(models.Model):
     #this django slugField adheres slug format
     slug=models.SlugField(unique=True)   
     description=models.TextField()
-    image=models.ImageField(upload_to="images")
+    image=models.ImageField(upload_to="images") 
+    location=models.ForeignKey(Location,  on_delete=models.CASCADE)  #CASCADE in case we delete foreign key or data related with this table will be deleted automatically  not this only option and also you can use ondelete=SET_NULL
 
 
     def __str__(self):
